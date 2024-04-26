63f24662c9116cfbcf3e5549bf79e17581c2e23f
dimpal001 <dimpaldas518@gmail.com>
2024-04-27 01:30:15 +0530

    update email page

--- utils/registrationVerificationMail.js
+++ utils/registrationVerificationMail.js
@@ -16,7 +16,8 @@ const sendVerificationEmail = async (email, token) => {
       from: process.env.EMAIL,
       to: email,
       subject: 'Account Verification - Solve Litigation',
-      html: `<!DOCTYPE html>
+      html: `
+      <!DOCTYPE html>
       <html lang="en">
         <head>
           <meta charset="UTF-8" />
@@ -33,33 +34,37 @@ const sendVerificationEmail = async (email, token) => {
             }
       
             h1 {
-              color: #0056b3;
               padding-bottom: 20px;
+              font-size: 40px;
             }
       
             /* Container styles */
             .container {
               max-width: 600px;
-              margin: 0 auto;
-              padding: 20px;
-              padding-top: 70px;
-              padding-bottom: 70px;
-              text-align: center;
+              margin: 20px auto;
+              padding: 30px;
+              padding-top: 50px;
+              padding-bottom: 50px;
               border: 1px solid #e0e0e0;
               border-radius: 8px;
             }
       
             /* Button styles */
             .button {
-              display: inline-block;
               font-size: 16px;
-              margin-bottom: 10px;
+              font-weight: bold;
+              margin-top: 15px;
+              margin-bottom: 15px;
+              border-color: transparent;
               padding: 12px 24px;
-              margin-top: 20px;
               background-color: #007bff;
               color: white;
               text-decoration: none;
               border-radius: 2px;
+            }
+      
+            .text {
+              font-size: 14px;
             }
       
             /* Button hover effect */
@@ -72,19 +77,21 @@ const sendVerificationEmail = async (email, token) => {
           <div class="container">
             <h1>Email Verification</h1>
             <p>Dear User,</p>
-            <p>
+            <p class="text">
               Thank you for signing up with <strong>Solve Litigation</strong>. Please
               click the button below to verify your email address:
             </p>
-            <a href="https://www.solvelitigation.com/verify-email/${token}" class="button"
-              >Verify Email</a
+            <a href="https://www.solvelitigation.com/verify-email/${token}"
+              ><button class="button">Verify Email</button></a
             >
-            <p>Best regards,<br /><strong>Solve Litigation</strong></p>
+            <p style="margin-bottom: 20px">
+              Best regards,<br /><strong>Solve Litigation</strong>
+            </p>
           </div>
         </body>
       </html>
       
-            `,
+      `,
     }
 
     await transporter.sendMail(mailOptions)
@@ -112,70 +119,79 @@ const sendResetPasswordEmail = async (email, token) => {
       html: `
       <!DOCTYPE html>
       <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Email Verification</title>
-        <style>
-          /* Reset styles */
-          body,
-          h1,
-          p {
-            margin: 0;
-            padding: 0;
-            font-family: Arial, sans-serif;
-          }
-    
-          h1 {
-            color: #0056b3;
-            padding-bottom: 20px;
-          }
-    
-          /* Container styles */
-          .container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            padding-top: 70px;
-            padding-bottom: 70px;
-            text-align: center;
-            border: 1px solid #e0e0e0;
-            border-radius: 8px;
-          }
-    
-          /* Button styles */
-          .button {
-            display: inline-block;
-            font-size: 16px;
-            margin-bottom: 10px;
-            padding: 12px 24px;
-            margin-top: 20px;
-            background-color: #007bff;
-            color: white;
-            text-decoration: none;
-            border-radius: 2px;
-          }
-    
-          /* Button hover effect */
-          .button:hover {
-            background-color: #0056b3;
-          }
-        </style>
-      </head>
-      <body>
-        <div class="container">
-          <h1>Reset Password</h1>
-          <p>Dear User,</p>
-          <p>
-            Click on the button below to reset your password
-          </p>
-          <a href="https://www.solvelitigation.com/reset-password/${token}" class="button"
-            >Reset Password</a
-          >
-          <p>Best regards,<br /><strong>Solve Litigation</strong></p>
-        </div>
-      </body>
-    </html>`,
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Email Verification</title>
+          <style>
+            /* Reset styles */
+            body,
+            h1,
+            p {
+              margin: 0;
+              padding: 0;
+              font-family: Arial, sans-serif;
+            }
+      
+            h1 {
+              padding-bottom: 20px;
+              font-size: 40px;
+            }
+      
+            /* Container styles */
+            .container {
+              max-width: 600px;
+              margin: 20px auto;
+              padding: 30px;
+              padding-top: 50px;
+              padding-bottom: 50px;
+              border: 1px solid #e0e0e0;
+              border-radius: 8px;
+            }
+      
+            /* Button styles */
+            .button {
+              font-weight: bold;
+              display: inline-block;
+              font-size: 16px;
+              margin-bottom: 10px;
+              padding: 12px 24px;
+              margin-top: 20px;
+              background-color: #007bff;
+              color: white;
+              text-decoration: none;
+              border-radius: 2px;
+            }
+      
+            .text {
+              font-size: 14px;
+            }
+      
+            /* Button hover effect */
+            .button:hover {
+              background-color: #0056b3;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>Reset Password</h1>
+            <p>Dear User,</p>
+            <p class="text">
+              A password reset has been requested for your account. If this was you,
+              please use the link below to reset your password.
+            </p>
+            <a
+              href="https://www.solvelitigation.com/reset-password/${token}"
+              class="button"
+              >Reset Password</a
+            >
+            <p>Best regards,<br /><strong>Solve Litigation</strong></p>
+          </div>
+        </body>
+      </html>
+      
+      `,
     }
 
     await transporter.sendMail(mailOptions)
